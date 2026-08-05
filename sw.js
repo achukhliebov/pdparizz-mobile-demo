d30d4dc500623a17d0fe16905af37d4f3c8182cb
achukhliebov <a.chukhliebov@gmail.com>
2026-08-05 23:30:30 +0300

    Refresh offline cache for conversion funnel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v1";
+const CACHE_NAME = "pdparizz-shell-v2";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
