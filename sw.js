5a940b6e2b74cac7a1e4c2818ef14fc44c163357
achukhliebov <a.chukhliebov@gmail.com>
2026-08-05 23:45:45 +0300

    Refresh offline cache for product variants

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v2";
+const CACHE_NAME = "pdparizz-shell-v3";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
