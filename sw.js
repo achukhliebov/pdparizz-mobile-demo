d8a5a124f9e7c993fbab8d6a8b62e078822c24dd
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:17:15 +0300

    Add complete subscription trial and management flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v8";
+const CACHE_NAME = "pdparizz-shell-v9";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
