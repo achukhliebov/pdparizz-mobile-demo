0a2cc878c264545f3d0ba2eda69f65f674c8563e
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:01:29 +0300

    Refresh personalized home cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v6";
+const CACHE_NAME = "pdparizz-shell-v7";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
