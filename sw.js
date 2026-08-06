4bf8eb04e2f8241f469aa13cf9e592fdab25ad3c
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:47:26 +0300

    Add campaign conversion funnel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v12";
+const CACHE_NAME = "pdparizz-shell-v13";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
