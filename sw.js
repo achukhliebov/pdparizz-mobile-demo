d4da30a43cdcef4bbdf45e3b23a81634c9ad4681
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:57:50 +0300

    Add partner presentation hub

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v14";
+const CACHE_NAME = "pdparizz-shell-v15";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
