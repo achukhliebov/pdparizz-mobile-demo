a0f587576ba8ad6c05e46d0f693200c0fa658e43
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:52:21 +0300

    Add offline data sync readiness

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v13";
+const CACHE_NAME = "pdparizz-shell-v14";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
