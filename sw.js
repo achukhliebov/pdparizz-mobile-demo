41f3af434109d2b4075b2cef487e4a80691c996b
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:24:00 +0300

    Build transparent daily prize wheel flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v9";
+const CACHE_NAME = "pdparizz-shell-v10";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
