9cfa294a3c495ffd292c8a5ede21818d841a8820
achukhliebov <a.chukhliebov@gmail.com>
2026-08-06 22:31:25 +0300

    Build end-to-end referral reward funnel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v10";
+const CACHE_NAME = "pdparizz-shell-v11";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
