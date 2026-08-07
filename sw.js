2d9eb168d3e32c3cc0f13bf0df8ea3d1274bb755
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 14:02:58 +0300

    Connect Bitrix24 fixture to full purchase flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v19";
+const CACHE_NAME = "pdparizz-shell-v20";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
