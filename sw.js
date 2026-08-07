586d5a3770fe268ea554e41421e9d493a7b07e8a
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 14:11:26 +0300

    Expose KIT order adapter contract

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v20";
+const CACHE_NAME = "pdparizz-shell-v21";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
