d2df8953831d7669b738b917590b43974ab6e55c
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 13:53:13 +0300

    Show Bitrix24 catalog adapter validation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v18";
+const CACHE_NAME = "pdparizz-shell-v19";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
