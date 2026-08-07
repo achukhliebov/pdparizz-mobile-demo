5a69d45188ff21db794c6a362b2ee1482d1bd549
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 16:57:03 +0300

    Add interactive catalog filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v30";
+const CACHE_NAME = "pdparizz-shell-v31";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
