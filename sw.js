76f352698b23f9a44dd93e208cdefc9e2f3f7bda
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 17:14:52 +0300

    Add product sharing and deep links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v32";
+const CACHE_NAME = "pdparizz-shell-v33";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
