57db3d1d9d638224a03bdaf3d915014c2cf254b8
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 14:48:02 +0300

    Add reliable integration delivery

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v23";
+const CACHE_NAME = "pdparizz-shell-v24";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
