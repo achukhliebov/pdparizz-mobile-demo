e967b4c9c84e39e9b184abf80721a7edbcfbc28b
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 14:56:44 +0300

    Add PostgreSQL reliability contract

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v24";
+const CACHE_NAME = "pdparizz-shell-v25";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
