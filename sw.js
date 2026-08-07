1c3d0b40cb47f78b5b3c08c238e337cb872176e0
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 17:05:32 +0300

    Add price watch and return notifications

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v31";
+const CACHE_NAME = "pdparizz-shell-v32";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
