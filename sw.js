8056dd3c7788999228d3503ed12808eeff4f860a
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 14:30:08 +0300

    Expose delivery adapter contract

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v22";
+const CACHE_NAME = "pdparizz-shell-v23";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
