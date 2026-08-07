80938719d2891802b5cb4bd3019f42e94d683ffd
achukhliebov <a.chukhliebov@gmail.com>
2026-08-07 08:49:55 +0300

    Fix stable demo link copying

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v16";
+const CACHE_NAME = "pdparizz-shell-v17";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
