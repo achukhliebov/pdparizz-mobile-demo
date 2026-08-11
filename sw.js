6ad83c081ffc85a4edc832c806cc01717d6e5d96
achukhliebov <a.chukhliebov@gmail.com>
2026-08-11 14:54:06 +0300

    Refresh Tiffany beta cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v40-checkout";
+const CACHE_NAME = "pdparizz-shell-v41-checkout";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
