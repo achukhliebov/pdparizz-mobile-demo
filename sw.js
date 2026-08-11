690fd012403ccca0c3c7759042bba69845b60e55
achukhliebov <a.chukhliebov@gmail.com>
2026-08-11 15:39:15 +0300

    Keep perfume volumes as separate cart lines

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v42-orders";
+const CACHE_NAME = "pdparizz-shell-v43-cart-variants";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
