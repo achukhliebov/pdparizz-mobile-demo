d542234c027e00a6a33648f03d664cb9c27da2fa
achukhliebov <a.chukhliebov@gmail.com>
2026-08-11 13:08:56 +0300

    Refine Tiffany catalog and product UX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pdparizz-shell-v37-tiffany";
+const CACHE_NAME = "pdparizz-shell-v39-uiux";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
